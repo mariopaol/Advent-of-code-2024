929debdfd0d6858f21b31effe5595fff804a8162
mariopaol <mariopaol@gmail.com>
2025-10-15 12:20:34 +0200

    Comentarios añadidos

--- day2/day2_1.py
+++ day2/day2_1.py
@@ -1,26 +1,24 @@
+# Abrimos el fichero input y lo guardamos datos. Como usamos readlines() se guardará como una lista en la que cada elemento es una linea
 with open('day2\input.txt','r') as entrada:
     datos = entrada.readlines()
 
-crece = 0
+# Variables globales
 valida = 0
 
 for line in datos:
-    report = line.split()
-    #print(report)
+    report = line.split()   # Separamos en cada espacio. Se guardará como una lista con strings separados por cada espacio
     crece = 0
-    for i in range (len(report)):
+    
+    for i in range (len(report)):   # Este bucle es para convertir los strings en int, así tenemos la lista con valores numéricos
         report[i] = int(report[i])
     for i in range (len(report)-1):
-        if abs(report[i] - report[i+1]) <= 3:
-            if report[i] - report[i+1] > 0:
+        if abs(report[i] - report[i+1]) <= 3:   # Queremos que la diferencia sea como máximo 3, como especifíca el ejercicio
+            if report[i] - report[i+1] > 0:     # Si el elemento i+1 es más grande que el i, lo contamos como creciente
                 crece +=1
-            elif report[i]-report[i+1] < 0:
+            elif report[i]-report[i+1] < 0:     # Si el elemento i+1 es más pequeño que el i, lo contamos como decreciente
                 crece -=1
         else:
-            #print('Hay una diferencia mayor que 3.')
             break
-        #print(crece)
-        if crece == len(report)-1 or crece == -len(report)+1:
-            #print('línea válida')
+        if crece == len(report)-1 or crece == -len(report)+1:   # Necesitamos que toda la lista sea creciente o decreciente para que sea válida. No valen iguales
             valida += 1
 print(valida)
--- day2/day2_2.py
+++ day2/day2_2.py
@@ -1,10 +1,12 @@
+# Abrimos el fichero input y lo guardamos datos. Como usamos readlines() se guardará como una lista en la que cada elemento es una linea
 with open('adventofcode\day2\input.txt','r') as entrada:
     datos = entrada.readlines()
 
+# Variables globales
 valida = 0
-# Vamos a probar a sacar los elementos 1 a 1 a ver si con eso se soluciona el error. Para ello vamos a hacer una función
 
-# Esta función está probada y ya funciona correctamente
+# La versión anterior del ejercicio no nos sirve para esta segunda parte. Vamos a hacer funciones para resolverlo
+# Esta función hace lo que el ejercicio 1. Te dice si una lista es monótona (creciente o decreciente) y sus elementos se alejan como máximo en 3
 def report_valido(list):
     crece = 0
     for i in range (len(list)-1):
@@ -18,7 +20,7 @@ def report_valido(list):
     if crece == len(list)-1 or crece == -len(list)+1:
         return True
 
-# Vamos a hacer que funcione esta otra función
+# Eliminamos los elementos de cada report de uno en uno y comprobamos si con ese cambio el report es válido. Esto es el dampener del problema 2
 def dampener(list):
     for i in range (len(list)):
         lista_auxiliar = list.copy()
@@ -29,12 +31,11 @@ def dampener(list):
         else:
             continue
 
-
-
+# Ahora hacemos las llamadas a las funciones
 for line in datos:
-    report = line.split()
+    report = line.split()   # Separamos en cada espacio. Se guardará como una lista con strings separados por cada espacio
     crece = 0
-    for i in range (0,len(report)):
+    for i in range (0,len(report)):     # Este bucle es para convertir los strings en int, así tenemos la lista con valores numéricos
         report[i] = int(report[i])
     if report_valido(report) == True:
         valida += 1
